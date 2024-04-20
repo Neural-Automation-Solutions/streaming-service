8edd79994c7b3130cc43ba8e5b8bd476d683bb20
yiannisha <hadjiyiannis.3@gmail.com>
2024-04-20 17:50:49 +0300

    Add main functionality

---
+++ main.py
@@ -0,0 +1,59 @@
+import time
+from http.client import HTTPSConnection
+import threading
+from videolib.video import Video
+
+# Initialize video
+vid = Video()
+
+# Set up a persistent HTTPS connection
+url = '/stream/test-stream'
+headers = {'Content-Type': 'image/jpeg'}
+
+# Stack to hold frames
+frame_stack = []
+
+# Lock to manage access to the frame stack
+lock = threading.Lock()
+
+def send_frame():
+    conn = None
+
+    def ensure_connection():
+        nonlocal conn
+        if conn is None or conn.sock is None:
+            conn = HTTPSConnection('dashboard.nas-ai.org')
+
+    while True:
+        frame = None
+        lock.acquire()
+        if frame_stack:
+            frame = frame_stack.pop()  # Get the most recent frame
+        lock.release()
+
+        if frame is not None:
+            try:
+                ensure_connection()
+                conn.request('POST', url, frame, headers)
+                response = conn.getresponse()
+                if response.status == 200:
+                    print('Frame sent successfully.')
+                else:
+                    print(f'Failed to send frame. Status code: {response.status}')
+                response.read()  # Important to read response to free up the connection
+            except Exception as e:
+                print(f'Error occurred while sending frame to server: {e}')
+                conn.close()
+                conn = None  # Force reconnection on next iteration
+
+# Start a thread for sending frames
+thread = threading.Thread(target=send_frame)
+thread.daemon = True
+thread.start()
+
+# Main loop to capture frames
+while True:
+    frame = vid.get_frame_bytes()
+    lock.acquire()
+    frame_stack = [frame]  # Reset the stack with the most recent frame
+    lock.release()
